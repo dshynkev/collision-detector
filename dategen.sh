5b890cab7717293fb3bd1c031ce20ade2926be9c
principio <lorem.ipsum.989@gmail.com>
2015-05-28 08:20:12 -0400

    Date generation fixed: timestamp is now preserved

--- dategen.sh
+++ dategen.sh
@@ -2,7 +2,13 @@
 
 for i in *.py; do
 	if grep -q -i 'LAST EDIT' ${i}; then
+		touch -r ${i} __stamp
 		mod_date=$(stat ${i} -c '%.19y')
 		sed -i.bak -e "s/LAST\ EDITED:*/LAST\ EDITED:\t${mod_date}/" ${i}
+		touch -r __stamp ${i}
 	fi
 done
+
+if [ -e __stamp ]; then
+	rm __stamp;
+fi
