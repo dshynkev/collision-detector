b1c0e57196d8aaac711f9a21ba3c0c22b8da2730
Dmytro Shynkevych <dm.shynk@gmail.com>
2017-03-20 21:44:51 -0400

    Changed all collisions to green for better visibility

--- __main__.py
+++ __main__.py
@@ -2,7 +2,7 @@
 AUTHOR:         principio
 LAST EDITED:	2016-02-11 19:12:20
 DESCRIPTION:    This is the main class of a simple collision detection demo
-                written in Python (Pyglet framework). 
+                written in Python (Pyglet framework).
 KNOWN ISSUES:   *> Segfaults on the only Windows machine I have at my disposal. Appears to be a Python issue.
 '''
 
@@ -37,11 +37,11 @@ class MainWindow(pyglet.window.Window):
         self.items = []             # All items rendered within this window
         self.dragged_items = []     # Items being dragged at the moment
         self.move_vectors = {}      # Vectors for random movement
-        
+
         self.fullscreen_flag = False
         self.multidrag_flag = True  # Whether all overlapping items or only the uppermost one should be dragged.
         self.random_move_flag = False # Whether shapes should be automatically moved
-        
+
         self.creation_flags = const.CREATE_NONE
         # This will be popped off when needed
         self.push_handlers(on_mouse_press = self.select_items,
@@ -56,7 +56,7 @@ class MainWindow(pyglet.window.Window):
             self.check_collisions([item])
         else:
             raise NotImplementedError("Only Shape subclasses can be added to this scene.")
-            
+
     def update_buttons(self):
         width = self.width*const.BUTTON_WIDTH_FACTOR
         height = self.height*const.BUTTON_HEIGHT_FACTOR
@@ -65,21 +65,21 @@ class MainWindow(pyglet.window.Window):
               Button('Add polygons', 2*width, 0, width, height, self.begin_creation, [const.CREATE_POLY], self.end_creation),
               Button('Enable random motion', 3*width, 0, width, height, self.toggle_random, [], self.toggle_random, []),
               Button('Enable multidrag', 4*width, 0, width, height, self. toggle_multidrag, [], self.toggle_multidrag, [], True)]
-    
+
     def check_collisions(self, items):
         for item in items:
             item.updateCollisions(self.items)
-        
+
     def toggle_random(self):
         self.random_move_flag = not self.random_move_flag
         if(self.random_move_flag):
             pyglet.clock.schedule(self.random_move)
         else:
             pyglet.clock.unschedule(self.random_move)
-            
-    def toggle_multidrag(self): 
+
+    def toggle_multidrag(self):
         self.multidrag_flag = not self.multidrag_flag
-        
+
     # Move items randomly by pre-generated vectors.
     def random_move(self, dt):
         for item in self.items:
@@ -89,51 +89,51 @@ class MainWindow(pyglet.window.Window):
                 # If the vector does not exist yet, create it
                 vector = geometry.Vector.fromTuple(helpers.getRandomTranslation())
                 self.move_vectors[id(item)] = vector
-             
+
             # IF the vector has been shortened by too much, make a new one
             if(vector.length < const.AUTO_SPEED):
                 vector = geometry.Vector.fromTuple(helpers.getRandomTranslation())
                 self.move_vectors[id(item)] = vector
-            
-            # Current transition vector is computed from the direction of the 
+
+            # Current transition vector is computed from the direction of the
             # principal transition vector and predefined speed
             current_trans = vector.normalized() * const.AUTO_SPEED
             item.moveBy(current_trans)
             # We decrement the translation vector by current vector
             vector.shortenBy(current_trans)
-        
+
         self.check_collisions(self.items)
-            
+
     def on_draw(self):
         self.clear()
         for button in self.buttons:
             button.render()
         for item in self.items:
             item.render()
-            
+
     def on_resize(self, width, height):
         # Resize GL viewport
         gl.glViewport(0, 0, width, height)
         gl.glMatrixMode(gl.GL_PROJECTION)
         gl.glLoadIdentity()
         gl.glOrtho(0, width, 0, height, -1, 1)
-        gl.glMatrixMode(gl.GL_MODELVIEW) 
-        
+        gl.glMatrixMode(gl.GL_MODELVIEW)
+
         # Inform all items of the new dimensions and adjust their positions accordingly
         Shape.tellScreenBounds(width, height)
         for item in self.items:
             item.adjustBounds()
-            
+
         # Update button positions: they are supposed to be dynamically sized
         self.update_buttons()
-        
+
     def check_buttons(self, x, y, button, modifiers):
-        status = False      
+        status = False
         if(button ==  mouse.LEFT):
             for button in self.buttons:
                 status |= button.tellClicked(x, y)
             if(status):
-                return True     # Don't let anyone else handle this event            
+                return True     # Don't let anyone else handle this event
 
     # Builds a list of items that could be dragged after this mouse_press.
     # Caveat: only these items are deemed to be "selected", i.e. deletion etc
@@ -144,7 +144,7 @@ class MainWindow(pyglet.window.Window):
                 self.dragged_items.append(item)
                 if(not self.multidrag_flag):    # If no multidrag, return when found the uppermost selected item
                     return True
-    
+
     # Moves selected items with the cursor. Performance would benefit from moving the
     # collision detection to on_draw(), since no one cares about collisions unless
     # they are indicated during rendering, but there is a _miniscule_ chance
@@ -156,7 +156,7 @@ class MainWindow(pyglet.window.Window):
 
     # Clears the list of items
     def release_items(self, x, y, button, modifiers):
-        if(button ==  mouse.LEFT):        
+        if(button ==  mouse.LEFT):
             self.dragged_items.clear()
 
     # Handle key presses.
@@ -169,19 +169,19 @@ class MainWindow(pyglet.window.Window):
             # On CTRL+Q, exit
             if(symbol == key.Q):
                 window.dispatch_event('on_close')
-                
+
         # On Delete, remove all items that are currently being dragged
         if(symbol == key.DELETE):
             for item in self.dragged_items:
                 self.items.remove(item)
             self.check_collisions(self.items)
-            
+
         # Dispatch default closing event on Escape.
         if(symbol == key.ESCAPE):
             window.dispatch_event('on_close')
-            
+
     def begin_creation(self, shape):
-        # Pop all handlers off and delete previous item reference  
+        # Pop all handlers off and delete previous item reference
         try:
             while 1:
                 self.pop_handlers()
@@ -191,7 +191,7 @@ class MainWindow(pyglet.window.Window):
             del self.temp_item
         except:
             pass
-        
+
         # Set the flag and push appropriate handlers
         self.creation_flags = shape
         if(self.creation_flags == const.CREATE_RECT):
@@ -204,10 +204,10 @@ class MainWindow(pyglet.window.Window):
                                        on_mouse_release = self.circle_on_release)
         elif(self.creation_flags == const.CREATE_POLY):
             self.push_handlers(on_mouse_press = self.polygon_on_click)
-            
+
         # Push UI handlers on the top
         self.push_handlers(on_mouse_press = self.check_buttons)
-    
+
     def end_creation(self):
         # Complete the polygon
         if(self.creation_flags == const.CREATE_POLY):
@@ -227,13 +227,13 @@ class MainWindow(pyglet.window.Window):
         # Push UI handler
         self.push_handlers(on_mouse_press = self.check_buttons)
         self.creation_flag = const.CREATE_NONE
-            
+
     # ~~~~~~~~~~ CIRCLE creation subroutines ~~~~~~~~~~
     def circle_on_click(self, x, y, button, modifiers):
         if(button == mouse.LEFT):
             self.temp_item = Circle(geometry.Point(x, y), 0)
             self.add_item(self.temp_item)
-        
+
     def circle_on_drag(self, x, y, dx, dy, buttons, modifiers):
         try:
             # Radius is the distance between the starting point and current point
@@ -242,7 +242,7 @@ class MainWindow(pyglet.window.Window):
             self.check_collisions([self.temp_item])
         except AttributeError:
             pass
-        
+
     def circle_on_release(self, x, y, button, modifiers):
         try:
             if(button == mouse.LEFT):
@@ -252,13 +252,13 @@ class MainWindow(pyglet.window.Window):
                 del self.temp_item
         except AttributeError:
             pass
-        
+
     # ~~~~~~~~~~ RECTANGLE creation subroutines ~~~~~~~~~~
     def rect_on_click(self, x, y, button, modifiers):
         if(button == mouse.LEFT):
             self.temp_item = Polygon.fromRectangle(geometry.Point(x, y), 0, 0)
             self.add_item(self.temp_item)
-    
+
     def rect_on_drag(self, x, y, dx, dy, buttons, modifiers):
         try:
             self.temp_item.width += dx
@@ -268,9 +268,9 @@ class MainWindow(pyglet.window.Window):
             self.check_collisions([self.temp_item])
         except AttributeError:
             pass
-        
+
     def rect_on_release(self, x, y, button, modifiers):
-        try:            
+        try:
             if(button == mouse.LEFT):
                 # Don't store zero-width/height rectangles
                 if (self.temp_item.width == 0 or self.temp_item.height == 0):
@@ -279,7 +279,7 @@ class MainWindow(pyglet.window.Window):
                 return True
         except AttributeError:
             pass
-        
+
     # ~~~~~~~~~~ POLYGON creation subroutines ~~~~~~~~~~
     def polygon_on_click(self, x, y, button, modifiers):
         if(button == mouse.LEFT):
@@ -289,7 +289,7 @@ class MainWindow(pyglet.window.Window):
             else:
                 self.temp_item.add_point(geometry.Point(x, y))
             self.check_collisions([self.temp_item])
-        
+
 if (__name__=="__main__"):
     window = MainWindow()
     pyglet.app.run()
--- shape.py
+++ shape.py
@@ -2,7 +2,7 @@
 AUTHOR:          principio
 LAST EDITED:	2016-02-11 19:12:20
 DESCRIPTION:     Shape parentclass. All shapes should subclass this.
-KNOWN ISSUES:   
+KNOWN ISSUES:
 '''
 import constants as const
 import geometry
@@ -10,30 +10,30 @@ import geometry
 class Shape:
     # This implements general collision-related abstractions and 'declares'
     # methods that will be invoked from the main module and, therefore, have to be implemented.
-    
+
     #These are specific to a particular shape and are implemeted in subclasses.
     '''
     def contains(self, point)               #[V] Will be pulled in from geometry.*
     def render(self)
     def collidingWith(self, item)
-    '''   
-    
+    '''
+
     # Every item is defined by at least a pair of coords.
     # These coords and width and height, as far as this class is concerned,
     # delimit a bounding rect for this shape.
-    # Generally, all shape-independent stuff goes here.    
+    # Generally, all shape-independent stuff goes here.
     def __init__(self, origin, width, height):
         self.bounds = geometry.Rectangle(origin, width, height)
-        
+
         self.collisions=[]
         self.colliding = const.COLLISION_NONE
-    
+
     @classmethod
     def tellScreenBounds(this, width, height):
         this.SCENE_START_HEIGHT = height*const.BUTTON_HEIGHT_FACTOR
         this.SCENE_WIDTH=width
         this.SCENE_HEIGHT=height
-        
+
     # For an arbitrary 2D shape, parallel translation by a vector is well-defined.
     # moveTo(point), however, is not (what is the anchor point of a pentagon, for instance?)
     # Therefore, external code can only use this subroutine for shape translation.
@@ -42,48 +42,48 @@ class Shape:
         if(self.bounds.width > self.SCENE_WIDTH
             or self.bounds.height > (self.SCENE_HEIGHT-self.SCENE_START_HEIGHT)):
             return False
-        
+
         self += trans_vector      # Move the item itself
         self.bounds += trans_vector # Move bounding rect accordingly
         self.adjustBounds()             # Make sure the shape stays in current window
-        
-    
+
+
     def adjustBounds(self):
-        adjust_vector = geometry.Vector(0, 0)    
-        
+        adjust_vector = geometry.Vector(0, 0)
+
         # Lower/left edges
         if(self.bounds.x<0):
             adjust_vector.x = -self.bounds.x
         if(self.bounds.y<self.SCENE_START_HEIGHT):
             adjust_vector.y = self.SCENE_START_HEIGHT-self.bounds.y
-        
+
         # Upper/right edges
-        x_overflow = self.bounds.x+self.bounds.width - self.SCENE_WIDTH 
+        x_overflow = self.bounds.x+self.bounds.width - self.SCENE_WIDTH
         y_overflow = self.bounds.y+self.bounds.height - self.SCENE_HEIGHT
         if(x_overflow>0):
             adjust_vector.x = -x_overflow
         if(y_overflow>0):
             adjust_vector.y = -y_overflow
-        
-        if(not adjust_vector.isNullVector()):   
+
+        if(not adjust_vector.isNullVector()):
             self.moveBy(adjust_vector)
         return True
-    
-    
+
+
     def getCollidingItems(self, items):
-        collisions = []   
+        collisions = []
         for item in items:
             if item is not self:
                 collision_type = self.collidingWith(item)
                 if(collision_type != const.COLLISION_NONE):
                     collisions.append((item, collision_type))
         return collisions
-    
+
     # item informs us that it is colliding with self. Record that.
     def adviseCollision(self, item, coltype):
         if (item, coltype) not in self.collisions:
             self.collisions.append((item, coltype))
-        
+
     # Ditto, but this tells us that item is no longer colliding with self.
     def adviseNoCollision(self, item):
         # By iterating through all collision records,
@@ -91,12 +91,12 @@ class Shape:
             # Find the one where the items matches the advice sender
             if(collision[0] is item):
                 self.collisions.remove(collision)
-    
+
     # Get items currently colliding with self; update local item list accordingly;
-    # inform other items about the collision state.    
-    def updateCollisions(self, items):      
+    # inform other items about the collision state.
+    def updateCollisions(self, items):
         new_collisions=self.getCollidingItems(items)
-        
+
         for collision in self.collisions:
             if collision not in new_collisions:
                 collision[0].adviseNoCollision(self)
@@ -106,10 +106,10 @@ class Shape:
             if collision not in self.collisions:
                 collision[0].adviseCollision(self, collision[1])
                 self.collisions.append(collision)
-                
-    # Before actually rendering anything, set the colliding flag appropriately.       
+
+    # Before actually rendering anything, set the colliding flag appropriately.
     def set_colliding_flag(self):
         self.colliding = const.COLLISION_NONE
-        
+
         for item, coltype in self.collisions:
             self.colliding = coltype if coltype > self.colliding else self.colliding
